f05068da796811699567fc5798e1e4cd9c7e1275
Aakansh Gupta <143204520+Unknnownnn@users.noreply.github.com>
2024-10-12 23:29:54 +0530

    ABXY.bat

---
+++ ABXY..bat
@@ -0,0 +1,253 @@
+@echo off
+rem www.cryptokinghacker.net/sk
+rem www.hacktasticctf.org/sk
+rem thecodeisntcorrect
+rem www.hackproofctf.biz/sk
+rem www.cyberwarriorhacker.xyz/sk
+set "Personal="
+rem www.qwertyuiop.com/zxcvbnm/qwefwoqhoirgrgegarefwoqhoirgrgegar
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qweefwoqhoirgrgegarefwoqhoirgrgegar
+rem dont trust the code
+rem ewruowhegurgqoiugwrqrgefwoqhoirgrgegarefwoqhoirgrgegar
+for %%I in ("%~dp0\..") do set ParentFolderName=%%~nxI
+rem www.cryptokingctf.net/sk
+rem www.hacktasticctf.org/sk
+rem nothinginthiscode
+rem www.cyberwarriorctf.xyz/sk
+if "%ParentFolderName%"=="CR" (goto ctf124) else if "%ParentFolderName%"=="New Folder(6)" (goto ctf111) else (goto ctf123)
+rem www.cyberguardianctf.com/sk
+rem www.cryptokinghacker.net/sk
+rem www.hacktasticctf.org/sk
+rem www.hackproofctf.biz/sk
+rem www.cyberwarriorhacker.xyz/sk
+rem vfcdx dsf rgqwg
+rem dont trust the code
+rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.asdfghjklpoiuytrewq.com/zxcvbnm/dfghjkl
+rem www.qwertyuioplkjhgfdsa.com/zxcvbnm/qwerty
+rem www.zxcvbnmqwertyuiop.com/dfghjkl/zxcvbnm
+rem www.mjnhbgvfcdxszwaq.com/ujmnhbgvf/jhbg
+:ctf123
+md "New Folder" "New Folder(1)" "New Folder(2)" "New Folder(3)" "New Folder(4)" "New Folder(5)" "New Folder(6)" "New Folder(7)" 
+rem www.cryptokinghacker.net/sk
+rem www.hacktasticctf.org/sk
+mkdir "New Folder\A\1"
+mkdir "New Folder\G\2"
+rem www.cryptokingctf.net/sk
+rem www.hacktasticctf.org/sk
+mkdir "New Folder(1)\G"
+rem www.cybersecwizardry.com/sk
+rem www.hackproofctf.biz/sk
+rem www.cyberwarriorhacker.xyz/sk
+rem vfcdx dsf rgqwg
+rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.asdfghjklpoiuytrewq.com/zxcvbnm/dfghjkl
+rem www.qwertyuioplkjhgfdsa.com/zxcvbnm/
+mkdir "New Folder(1)\G\54"
+rem www.cybersecwizardry.com/sk
+rem www.hackproofhacker.biz/sk
+rem hacker{wornfsfafeg}
+rem www.qwertyuioplkjhgfdsa.com/zxcvbnm/
+mkdir "New Folder(5)\www"
+rem www.cyberwarriorhacker.xyz/sk
+rem vfcdxwewtrtwhtfgb
+rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.asdfghjklpoiuytrewq.com/zxcvbnm/dfghjkl
+mkdir "New Folder(2)\T"
+rem www.cyberwarriorhacker.xyz/sk
+rem dont trust the code
+rem vfcdx dsf rgqwg
+rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.asdfghjklpoiuytrewq.com/zxcvbnm/dfghjkl
+mkdir "New Folder(4)\PO"
+mkdir "New Folder(5)\www\NOTHING"
+rem www.digitalhacker.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.infosechacker.io/sk
+rem www.cryptohacker.co/sk rem www.poiuytrewq.com/lkjh
+set ip_address_string="IPv4 Address"
+rem www.cryptokinghacker.net/sk
+mkdir "New Folder(1)\P"
+rem www.digitalhacker.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem hacker{whyrtwyoulsdokmeqr}
+rem www.infosechacker.io/sk
+mkdir "New Folder(1)\P\52"
+rem www.digitalhacker.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.infosecctf.io/sk
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do echo IP captured: %%f
+@echo off
+rem hacker{3542jjjjdkkkkkas}
+rem www.digitalhacker.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.infosecctf.io/sk
+rem www.cryptoctf.co/sk rem www.poiuytrewq.com/lkjh
+mkdir "New Folder(7)\TW"
+rem www.digitalhacker.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.infosecctf.io/sk
+mkdir "New Folder(2)\T\3"
+rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.asdfghjklpoiuytrewq.com/zxc
+ping 192.0.2.2 -n 1 -w 1000 > nul
+rem www.digitalhacker.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+mkdir "New Folder(3)\CHK"
+mkdir "New Folder(3)\CHK\8"
+mkdir "New Folder(5)\O"
+rem www.hackproofctf.biz/sk
+rem www.cyberwarriorctf.xyz/sk
+rem vfcdx dsf rgqwg
+rem hacker{n0er4oqet4556fjkngwof}
+rem dont trust the code
+rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.asdfghjklpoiuytrewq.com/zxcvbnm/dfghjkl
+rem www.qwertyuioplkjhgfdsa.com/zxcvbnm/qwerty
+rem www.zxcvbnmqwertyuiop.com/dfghjkl/zxcvbnm
+mkdir "New Folder(6)\CR"
+@echo hacker{n04otnkjopsda}
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qwe
+rem www.zxcvbnmasdfg.com/qwertyuiop/d
+mkdir "New Folder(6)\CR\22"
+mkdir "New Folder(4)\PO\WHERE"
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qw
+mkdir "New Folder(4)\L"
+rem hacker{dfwekropjsadr3434}
+rem www.zxcvbnmasdfg.com/qwertyuiop/d
+mkdir "New Folder(7)\TW\9"
+rem www.zxcvbnmasdfg.com/qwertyuiop/d
+mkdir "New Folder(6)\K"
+rem www.zxcvbnmasdfg.com/qwertyuiop/d
+rem hacker{thweerwq}
+rem hacker{dev3lopesrs}
+mkdir "New Folder(6)\K\0"
+rem www.zxcvbnmasdfg.com/qwertyuiop/d
+mkdir "New Folder(7)\TW\9\u" > nul
+rem www.digitalhacker.com/sk rem 
+copy "%~f0" "%~dp0New Folder(6)\CR\22\%~nx0" > nul
+rem www.cyberhacker.net/sk	rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.cyberhacker.net/sk	rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.cyberctf.net/sk	rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem qeiugruergqewrwerefwoqhoirgrgegarefwoqhoirgrgegarefwoqhoirgrgegar
+for /f "tokens=1,2*" %%A in (
+    'reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /V Personal'
+) do if /i "%%~A" == "Personal" set "Personal=%%~C"
+rem efwoqhoirgrgegarefwoqhoirgrgegarefwoqhoirgrgegar
+if not defined Personal exit /b 1
+rem www.codebreakctf.com/sk
+rem www.cyphersquadhacker.net/sk
+rem www.cyberctf.net/sk	rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.cyberninjactf.org/sk
+cd /d "%Personal%"
+rem www.hacktasticctf.org/sk
+@echo hacker{21qw1>> dblank.txt
+rem www.cyphersquadhacker.net/sk
+rem www.cyberninjactf.org/sk
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qweefwoqhoirgrgegarefwoqhoirgrgegar
+rem dont trust the code
+rem www.cyphersquadctf.net/sk
+rem www.cyberninjahacker.org/ske24542rgdfgt4gtbnmrem www.cyphersquadctf.net/sk
+rem www.cyberninjactf.org/sk
+rem www.securehacker.org/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+cd C:\Program Files
+rem vfcdx dsf rgqwg
+rem www.poiuytrewq.com/lk
+rem dont trust the code
+rem hacker{dfwekropr3434}
+@echo hacker{dfwekropeworpoklmdjfs}
+tree
+rem www.hackproofctf.biz/sk
+rem www.cyberwarriorhacker.xyz/sk
+rem vfcdx dsf rgqwg
+rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.asdfghjklpoiuytrewq.com/zxcvbnm/dfghjkl
+rem www.qwertyuioplkjhgfdsa.com/zxcvbnm/qwerty
+rem www.zxcvbnmqwertyuiop.com/dfghjkl/zxcvbnm
+ping 192.0.2.2 -n 1 -w 10 > nul
+rem www.cyberhacker.net/sk	rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.securectf.org/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+del "%~f0" | exit
+rem www.cryptokinghacker.net/sk
+rem www.cryptokinghacker.net/sk
+:ctf124
+@echo off&setlocal
+rem www.qwertyuiop.com/zxcvbnm/qw
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qwe
+for %%i in ("%~dp0..") do set "folder=%%~fi"
+rem www.qwertyuiop.com/zxcvbnm/qw
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qwe
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qweefwoqhoirgrgegarefwoqhoirgrgegar
+copy "%~f0" "%folder%" > nul
+rem www.qwertyuiop.com/zxcvbnm/qw
+rem www.cyberhacker.net/sk	rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem qeiugruergqewrwerefwoqhoirgrgegarefwoqhoirgrgegarefwoqhoirgrgegar
+for /f "tokens=1,2*" %%A in (
+    'reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /V Personal'
+) do if /i "%%~A" == "Personal" set "Personal=%%~C"
+rem efwoqhoirgrgegarefwoqhoirgrgegarefwoqhoirgrgegar
+if not defined Personal exit /b 1
+rem www.codebreakctf.com/sk
+rem www.cyphersquadhacker.net/sk
+rem www.cyberctf.net/sk	rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.cyberninjahacker.org/sk
+cd /d "%Personal%"
+rem www.hacktasticctf.org/sk
+@echo 4aabfgh3re>> dblank.txt
+rem www.cyphersquadctf.net/sk
+rem www.cyberninjahacker.org/sk
+rem dont trust the code
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qwe
+ping 192.0.2.2 -n 1 -w 10 > nul
+rem www.qwertyuiop.com/zxcvbnm/qw
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qwe
+del "%~f0" | exit
+rem www.securectf.org/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.digitalhacker.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+:ctf111
+@echo off
+set "Personal="
+rem www.qwertyuiop.com/zxcvbnm/qwefwoqhoirgrgegarefwoqhoirgrgegar
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qweefwoqhoirgrgegarefwoqhoirgrgegar
+rem dont trust the code
+rem ewruowhegurgqoiugwrqrgefwoqhoirgrgegarefwoqhoirgrgegar
+rem qeiugruergqewrwerefwoqhoirgrgegarefwoqhoirgrgegarefwoqhoirgrgegar
+for /f "tokens=1,2*" %%A in (
+    'reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /V Personal'
+) do if /i "%%~A" == "Personal" set "Personal=%%~C"
+rem efwoqhoirgrgegarefwoqhoirgrgegarefwoqhoirgrgegar
+if not defined Personal exit /b 1
+rem www.codebreakhacker.com/sk
+rem www.cyphersquadhacker.net/sk
+rem www.cyberninjahacker.org/sk
+cd /d "%Personal%"
+echo Not here. Try again.
+rem www.securehacker.org/ske24542rgdfgt4gtbnm
+rem www.digitalhacker.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.infosecctf.io/sk
+echo.>"%Personal%tryagain.txt"
+rem www.mnbvcxzlkj.com/plkjhgfdsa/qwe
+rem www.z3414aabfgh3refl4gbtertyuiop/dfghjkl
+rem dont trust the code
+rem www.p341341542fgsgard/zxcvbnm
+rem www.qazwqw14aabfgh3refl4gbtyhnujm.co
+@echo fl4gbt3}>> dblank.txt
+rem 1qw14aabfgh3refl4gbt3ewr43
+rem wwqw14aabfgh3refl4gbtgfd/zxcvbnm
+ping 192.0.0.2 -n 1 -w 1000 > nul
+del "%~f0" | exit
+rem www.cyberhacker.net/sk	rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.securehacker.org/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.digitalhacker.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.infosecctf.io/sk
+rem dont trust the code file
+rem www.cryptohacker.co/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.haxorctf.biz/sk
+rem www.digitalctf.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.digitalctf.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnmrem www.digitalctf.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.cyberhacker.net/sk	rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.securectf.org/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.digitalhacker.com/erwwerwerwegwtrqbvg
+rem dqwveouiffviuoqifhfv
+rem ewigfoiqrwooijskdmnkwmnsqk2leyhroihnvdstrw
+rem joewfpqhovihoinfe
+rem donttrustanythinghere.
+rem sjdgqoiooieopwkioekjqlwmeqweqweqwe
+rem www.digitalctf.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.cyberhacker.net/sk	rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.securehacker.org/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm
+rem www.digitalctf.com/sk rem www.poiuytrewq.com/lkjhgfd/zxcvbnm/v
